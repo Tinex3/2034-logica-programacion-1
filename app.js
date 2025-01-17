8e5e2b539584633bf23511eceb06d3525b40ce0d
Benjamin Riquelme <b.riquelme.gomez@gmail.com>
2025-01-17 00:07:11 -0300

    Se crean mensajes por consola para depuracion

--- app.js
+++ app.js
@@ -1,16 +1,21 @@
 //Variables
 let numeroSecreto = 2;
-let numeroUsuario = prompt("Me indicas un número entre 1 y 10 por favor:");
 
-console.log(numeroUsuario);
+let numeroUsuario = prompt("Me indicas un número entre 1 y 10 por favor:");
+//Imprimimos el número que el usuario ingresó
+console.log("El valor del intento es" + numeroUsuario);	
+
 /*
 Este código realiza
+
 la comparación
 */
 if (numeroUsuario == numeroSecreto) {
     //Acertamos, fue verdadera la condición
+    console.log("Acertaste, el número es: " + numeroUsuario);
     alert(`Acertaste, el número es: ${numeroUsuario}`);
 } else {
+    console.log("Lo siento, no acertaste el número");
     //La condición no se cumplió
     alert('Lo siento, no acertaste el número');
 }
